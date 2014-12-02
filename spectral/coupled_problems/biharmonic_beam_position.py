e0a389d1cfdf5da9d5630274d61de678a500ff01
mirok <miroslav.kuchta@gmail.com>
2014-12-02 13:20:33 +0100

    work test config

--- spectral/coupled_problems/biharmonic_beam_position.py
+++ spectral/coupled_problems/biharmonic_beam_position.py
@@ -38,8 +38,8 @@ params = {'E_plate': 1.,
 # Checkout LBB for different positions of the beam
 # The beam position is given by A = A(s) = [0.5*s, 0],
 # B = B(t) = [0.5 + 0.5*t, 1]. Both s, t are in [0, 1]
-s = np.linspace(0, 1, 4)
-t = np.linspace(0, 1, 4)
+s = np.linspace(0, 1, 6)
+t = np.linspace(0, 1, 6)
 
 As = np.vstack([0.5*s, 0*np.ones_like(s)]).T
 Bs = np.vstack([0.5*t + 0.5, np.ones_like(t)]).T
@@ -67,8 +67,8 @@ fig.savefig('%s/biharmonic_positions.pdf' % results_dir)
 
 # Plot the eigenvalues of fixed A, that is each n_cols, into tiled plot
 # The number of rows and cols in this plots N_rows, N_cols
-N_cols = 2
-N_rows = n_cols // 2 if (n_cols % 2) == 0 else (n_cols // 2) + 1
+N_cols = 2         # Note that this is hardcoded to have N_cols*N_rows = n_cols
+N_rows = 3
 counter = 0
 figs = []
 eigen_data = {}
@@ -77,6 +77,9 @@ row_betas, row_angles = [], []
 for i, (A, B) in enumerate(product(As, Bs)):
     # Switch to new figure with new row
     if i % n_cols == 0:
+	if i:
+	    figs[-1].show()
+
         # Reset
         row_betas = []
         row_angles = []
@@ -99,7 +102,7 @@ for i, (A, B) in enumerate(product(As, Bs)):
     ns = []
     params['A'] = A
     params['B'] = B
-    for n in range(3, 5):
+    for n in range(3, 16):
         n_plate, n_beam, n_lambda = rule(n)
         params['n_plate'] = n_plate
         params['n_beam'] = n_beam
